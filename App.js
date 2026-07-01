213c54c365fdf742a4769840721dc3e6781dd422
Abhay Gupta <abhaygupta09583@gmail.com>
2026-07-01 12:45:38 +0530

    fix: ignore expo-notifications console warning logs in LogBox

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
+import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
+
+LogBox.ignoreLogs([
+  'expo-notifications: Android Push notifications',
+  '`expo-notifications` functionality is not fully supported in Expo Go'
+]);
 
 import { AuthProvider } from './src/context/AuthContext';
 import { CartProvider } from './src/context/CartContext';
